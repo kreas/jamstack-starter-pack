0737345351589b3e30f80081e14511390cbe5acf
Tim Warren <tim.warren@takl.com>
2020-03-01 12:34:37 -0600

    Added now with serverless functions

--- packages/site/src/routes/index.svelte
+++ packages/site/src/routes/index.svelte
@@ -1,16 +1,11 @@
 <style lang="postcss">
-#hero {
-  height: 100%;  
-}
 </style>
 
 <svelte:head>
-	<title>Tim Warren</title>
+	<title></title>
 </svelte:head>
 
-<div id="hero" class="text-center bg-gray-900 text-white">
-  <div class="content">
-    <span class="text-3xl">Hello, I'm <strong class="text-blue-500">Tim Warren</span>
-  </div>
+<div class="text-center mt-48">
+  <h1 class="text-3xl uppercase">Make cool shit</h1>
 </div>
 
